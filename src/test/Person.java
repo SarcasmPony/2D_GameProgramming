bad243826d1efdb5693c1464f8266d4cfc4f3c15
SarcasmPony <dominik-lehmann@hotmail.com>
2015-11-29 17:15:26 +0100

    Swag 2.0

---
+++ src/test/Person.java
@@ -0,0 +1,27 @@
+package test;
+
+
+public class Person
+{
+    String name;
+    
+    public Person(String name)
+    {
+        this.name = name;        
+    }
+    
+//    public static void main(String[] args)
+//    {
+//        Person[] family;
+//        Person[] family2;
+//        family = new Person[]{
+//            new Person("Peter"),
+//            new Person("Seter"),
+//            new Person("Samuel")
+//        };
+//        
+//        family2 = {new Person("Gabe")};
+//        
+//        System.out.print(family[1]);
+//    }
+}
